696589655f2aee673744e7d7636b13b8845f5c8f
Tanya Bihag <tbihag@gmail.com>
2019-08-26 00:32:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] = "C"
 end
 
 def adding_matz
@@ -80,7 +83,9 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = "{
+  :known_for => "Ruby",
+   :languages => ["LISP", "C"]}"
 end
 
 def changing_alan
