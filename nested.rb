13c9513323817c74b30ad2d94313faa87c508ef1
Tanya Bihag <tbihag@gmail.com>
2019-08-26 00:58:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
- programmer_hash.merge[:dennis_ritchie](:languages => {["C", "Assembly"]})
-end
+  programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+  programmer_hash
+end   
